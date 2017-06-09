0f92ff7c5e5969e99a71dacf4f5315547d26ee37
alexandre-w <alexandre.git67@gmail.com>
2017-06-09 21:34:17 +0200

    Ajoutez des fichiers projet.

---
+++ GigHubExercise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigHubExercise.Startup))]
+namespace GigHubExercise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
