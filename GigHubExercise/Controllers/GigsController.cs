a02862a66f41e8b59f6104d5861c11f0ebe4e416
alexandre-w <alexandre.git67@gmail.com>
2017-06-12 22:57:17 +0200

    Add a new Gig form

---
+++ GigHubExercise/Controllers/GigsController.cs
@@ -0,0 +1,33 @@
+﻿using GigHubExercise.Models;
+using GigHubExercise.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHubExercise.Controllers
+{
+    public class GigsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        /// <summary>
+        /// Create a new Gig
+        /// </summary>
+        /// <returns>GigFormViewModel</returns>
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ GigHubExercise/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GigHubExercise.Models;
+using System.Collections.Generic;
+
+namespace GigHubExercise.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
